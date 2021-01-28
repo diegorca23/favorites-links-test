70d8f635d5f4c3a867c8cd79753ab434a0222505
Diego Caceres <diego.rca.23@gmail.com>
2021-01-27 22:41:19 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const flash = require("connect-flash");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session");
+
+const { database } = require("./keys");
+
+//initializations
+const app = express();
+
+//setings
+app.set("port", process.env.PORT || 4000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(".hbs", exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    helpers: require("./lib/handlebars")
+}));
+app.set("view engine", ".hbs");
+
+//middlewares
+app.use(session({
+    secret: "faztmysqlnodesession",
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database)
+}));
+app.use(flash());
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+//global variables
+app.use((req, res, next) => {
+    app.locals.success = req.flash("success");
+    next();
+});
+
+//routes
+app.use(require("./routes"));
+app.use(require("./routes/authentications"));
+app.use("/links", require("./routes/links"));
+
+//public
+app.use(express.static(path.join(__dirname, "public")));
+
+//starting the server
+app.listen(app.get("port"), () => {
+    console.log("Serveron port", app.get("port"));
+});
---
+++ src/routes/links.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+
+const pool = require("../database");
+
+router.get("/add", (req, res) => {
+    res.render("links/add");
+});
+
+router.post("/add", async (req, res) => {
+    const { title, url, description } = req.body;
+    const newLink ={
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    req.flash("success", "Link saved succesfully");
+    res.redirect("/links");
+});
+
+router.get("/", async (req, res) => {
+    const links = await pool.query('SELECT * FROM links');
+    //console.log(links);
+    res.render("links/list", { links: links });
+});
+
+router.get("/delete/:id", async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+    req.flash("success", "Link deleted succesfully");
+    res.redirect("/links");
+});
+
+router.get("/edit/:id", async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id=?', [id]);
+    //console.log(links[0]);
+    res.render("links/edit", { link: links[0] });
+});
+
+router.post("/edit/:id", async (req, res) => {
+    const { id } = req.params;
+    const { title, url, description } = req.body;
+    const newLink ={
+        title,
+        url,
+        description
+    };
+    //console.log(newLink);
+    await pool.query( 'UPDATE links set ? WHERE id= ?', [newLink, id]);
+    req.flash("success", "Link updated succesfully");
+    res.redirect("/links");
+});
+
+module.exports = router;
