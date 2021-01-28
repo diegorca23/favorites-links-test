79f1e353700d50ec205896dec2bc287f2f091a9b
Diego Caceres <diego.rca.23@gmail.com>
2021-01-28 10:01:00 -0300

    Second commit - Users SignUp Added

--- src/routes/authentications.js
+++ src/routes/authentications.js
@@ -1,4 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+    res.render("auth/signup");
+});
+
+router.post("/signup", passport.authenticate("local.signup", {
+    successRedirect: "/profile",
+    failureRedirect: "/signup",
+    failureFlash: true
+}));
+
+    //console.log(req.body);
+    //res.send("received");
+
+
+router.get("/profile", (req, res) => {
+    res.send("This is your profile");
+});
+
+
+
 module.exports = router;
